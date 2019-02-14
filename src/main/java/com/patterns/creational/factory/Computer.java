905791193ea57645430d4620b43dd7a8ece49ce5
Murali Nutalapati <info@muraliprashanth.me>
2019-02-14 15:43:15 +0530

    factory pattern

---
+++ src/main/java/com/patterns/creational/factory/Computer.java
@@ -0,0 +1,14 @@
+package com.patterns.creational.factory;
+
+public abstract class Computer {
+
+  abstract String getRAM();
+  abstract String getHDD();
+  abstract String getCPU();
+
+  @Override
+  public String toString() {
+    return "RAM= " + this.getRAM() + ", HDD=" + this.getHDD() + ", CPU=" + this.getCPU();
+  }
+
+}
