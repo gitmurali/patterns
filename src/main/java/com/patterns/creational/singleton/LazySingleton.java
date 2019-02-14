8a01ad5568b01b52bb41686f11e8dad261322fc7
Murali Nutalapati <info@muraliprashanth.me>
2019-02-14 15:24:32 +0530

    singleton variations

---
+++ src/main/java/com/patterns/creational/singleton/LazySingleton.java
@@ -0,0 +1,15 @@
+package com.patterns.creational.singleton;
+
+public class LazySingleton {
+  private static LazySingleton lazySingleton;
+
+  private LazySingleton() {
+  }
+
+  public static LazySingleton getInstance() {
+    if (lazySingleton == null) {
+      lazySingleton = new LazySingleton();
+    }
+    return lazySingleton;
+  }
+}
